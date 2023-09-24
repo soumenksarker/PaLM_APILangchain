f63d87e61d0c68d8ec902da46b767cb3f4192131
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-24 21:43:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,8 @@ def main():
     # Initialize Streamlit
     st.sidebar.title("Document Processing")
     uploaded_files = st.sidebar.file_uploader("Upload multiple files texts, docx or pdfs", accept_multiple_files=True)
-    st.sidebar.title("News Article URLs")
-    st.sidebar.write("To extract info from multiple URL's, remove the previous one and paste a single URL each time! Info from new URL will be accommodated to the system automatically!")
+    st.sidebar.title("Enter URLs")
+    st.sidebar.write("To extract info from multiple URL's, remove the previous one and paste a new URL each time! Info from new URL will be accommodated to the system automatically!")
     urls = []
     for i in range(1):
         url = st.sidebar.text_input(f"URL {i+1}")
