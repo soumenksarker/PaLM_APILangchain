005f18743e342ca7cbdece1d087c48973d924d66
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-24 21:41:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def main():
         if len(urls)>0:
             # load data
             loader = UnstructuredURLLoader(urls=urls)
-            main_placeholder.text("Data Loading...Started...✅✅✅")
+            main_placeholder.text("System is loading ✅✅✅")
             text.extend(loader.load())
             urls=[]
             time.sleep(2)
@@ -139,9 +139,10 @@ def main():
                 if loader:
                     text.extend(loader.load())
                     os.remove(temp_file_path)
-        main_placeholder.text("Text Splitter...Started...✅✅✅")
+        main_placeholder.text("System is loading ✅✅✅")
         text_splitter =RecursiveCharacterTextSplitter(separators=['\n\n', '\n', '.', ','],chunk_size=700, chunk_overlap=150) 
         text_chunks = text_splitter.split_documents(text)
+        main_placeholder.text("System is Running, Interact! ✅✅✅")
         # Create the chain object
         chain=load_embedding(text_chunks)
         display_chat_history(chain)
