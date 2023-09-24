fe979aee26da555afa98f3ad4641342f4b3f7afa
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-24 17:43:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def main():
         st.write(len(query_result))
         # initialize pinecone
         pinecone.init(api_key="0fa06a79-cf08-484b-b91c-236b77956235",  # find at app.pinecone.io
-                      environment="gcp-starter"  # next to api key in console)
+                      environment="gcp-starter")  # next to api key in console)
         index_name = "langchainpinecone" # put in the name of your pinecone index here
 
         docsearch = Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
