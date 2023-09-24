90906a14177cbfcf748e546d50ef04a8fbc8fb70
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-24 18:26:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def create_conversational_chain(docsearch):
     chain_type_kwargs = {"prompt": prompt}
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
     return qa
-@st.cache
+@st.experimental_singleton
 def load_embedding(text_chunks, index_name):
     load_dotenv()
     # # Create embeddings
